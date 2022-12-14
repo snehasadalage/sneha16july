3c8a12497bc2a370e8a3347fe084f8abf7664395
sneha sadalage <snehasadalage34@gmail.com>
2022-12-14 13:44:06 +0530

    add spaces in program

--- src/test/java/neostoxbase/BaseNew.java
+++ src/test/java/neostoxbase/BaseNew.java
@@ -13,6 +13,12 @@ public class BaseNew
 	protected static WebDriver driver;
 	public void launchBrowser() throws InterruptedException, IOException
 	{
+		
+		
+		
+		
+		
+		
 	System.setProperty("webdriver.chrome.driver", "E:\\chromedriver_win32\\chromedriver.exe");
 	driver= new ChromeDriver();
 	driver.manage().window().maximize();
